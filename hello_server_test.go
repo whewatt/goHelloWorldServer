29cffda14270ffcb0656c70efacb195d83285df1
whewatt <weshewatt@gmail.com>
2025-09-20 16:25:43 -0400

    Hello version

--- hello_server_test.go
+++ hello_server_test.go
@@ -4,14 +4,14 @@ import "testing"
 
 func TestGreetingSpecificJohn(t *testing.T) {
 	greeting := CreateGreeting("John")
-	if greeting != "Welcome, John\n" {
+	if greeting != "Hello, John\n" {
 		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
 	}
 }
 
 func TestGreetingSpecificDemo(t *testing.T) {
 	greeting := CreateGreeting("Demo")
-	if greeting != "Welcome, Demo\n" {
+	if greeting != "Hello, Demo\n" {
 		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
 	}
 }
